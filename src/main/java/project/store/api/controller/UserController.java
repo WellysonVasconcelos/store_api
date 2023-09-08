a5de086831252fab3a1378da7ccfa65ea50f7b32
Wellyson Vasconcelos <wellysonvasc22@gmail.com>
2023-09-08 17:52:25 -0300

    add: request for create user

--- src/main/java/project/store/api/controller/UserController.java
+++ src/main/java/project/store/api/controller/UserController.java
@@ -1,18 +1,28 @@
 package project.store.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import project.store.api.user.User;
+import project.store.api.user.UserRepository;
 import project.store.api.user.userData;
 
 @RestController
 @RequestMapping("user")
 
 public class UserController {
-    @PostMapping
-    public void create(@RequestBody userData data){
 
-        System.out.println(data);
+    private final UserRepository repository;
+
+    public UserController(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping
+    public void create(@RequestBody userData data) {
+
+        repository.save(new User(data));
     }
 }
